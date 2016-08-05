fc3f53abe869d25c603c6046b99595194dc4ca4e
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-05 16:42:13 +0300

    attach solution for parethenses matcher

--- paren_matcher/paren_matcher.py
+++ paren_matcher/paren_matcher.py
@@ -6,3 +6,24 @@
 #   '((()'
 #   '())('
 
+
+braces = {'(': ')', '[': ']', '{': '}'}
+
+
+def paren_matcher(string_symbol):
+    stack = []
+    for bracket in string_symbol:
+        c = braces.get(bracket)
+        if c:
+            stack.append(c)
+        elif not stack or stack.pop() != bracket:
+            return False
+    return not stack
+
+print paren_matcher('((()))')
+print paren_matcher('[[][[')
+
+
+
+
+
