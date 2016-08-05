9c09dd359cef4f0e6e7a0348db6dd3e171203ae2
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-05 16:49:23 +0300

    add comments

--- password_generator/password.py
+++ password_generator/password.py
@@ -1,7 +1,11 @@
 import random
 
+# define the key for generating passwords
 key_ = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+
+# define passwords length
 pass_len = 10
 
+# generate password randomly
 password = "".join(random.sample(key_, pass_len))
 print password
